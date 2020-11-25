f4f0b5f5d02553b43a0870f74501825b2a6004fd
Rodrigo Portela da Costa <rpcosta@gmail.com>
2020-11-24 21:48:31 -0300

    Initial version

---
+++ src/main/java/com/aprimora/psico/psico/PsicoApplication.java
@@ -0,0 +1,13 @@
+package com.aprimora.psico.psico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aprimora/psico/psico/PsicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprimora.psico.psico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
